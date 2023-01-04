7e15625ad1cb41fbda1c60d156bdaf9745945585
Divyesh Shrestha <rashudshrestha12@gmail.com>
2023-01-04 13:13:42 +1100

    latest-push

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 import express from'express'
 import data from './data.js'
+import cors from 'cors'
 
 const app = express()
 
@@ -24,6 +25,7 @@ app.get ('/api/products/slug/:id', (req, res) =>{
     }
     res.send(data.products)
 })
+app.use(cors())
 const port = process.env.PORT || 5000
 app.listen(port, () =>{
     console.log(`serve at http://localhost:${port}`)
